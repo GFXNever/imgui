11cf94d38a4e3b569fa4f0fc65fbf8df6ae83cf3
Omer Cohen <nevergfx1@gmail.com>
2020-12-30 18:57:37 +0200

    Fixed premake

--- premake5.lua
+++ premake5.lua
@@ -12,11 +12,11 @@ project "ImGui"
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
 	}
 
 	filter "system:windows"
